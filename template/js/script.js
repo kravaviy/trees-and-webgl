e72df8907134a3bc3452df0dee3a82360ccf07cc
igor-krava <sasha_721a@mail.ru>
2018-01-15 01:36:53 +0300

    not portfolio this firsr work to create

---
+++ template/js/script.js
@@ -0,0 +1,221 @@
+;(function ($) {
+
+    function homepageResponsive() {
+
+        var windowsWidth = $(window).width(),
+        windowsHeight = $(window).height();
+
+        if (windowsWidth > windowsHeight) {
+
+            $('.introduction , .menu').css({
+                width: '50%',
+                height: '100%'
+            });
+
+        } else {
+
+            $('.introduction , .menu').css({
+                width: '100%',
+                height: '50%'
+            });
+
+        }
+
+        var introWidth = $('.introduction').width(),
+        introHeight = $('.introduction').height(),
+        bgImage = $('.introduction').find('img');
+
+        if (introWidth > introHeight) {
+
+            bgImage.css({
+                width: '100%',
+                height: 'auto'
+            });
+
+        } else {
+
+            bgImage.css({
+                width: 'auto',
+                height: '100%'
+            });
+
+        }
+
+    }
+
+    $(window).on('load resize', homepageResponsive);
+
+    $('.menu > div').on('click', function () {
+
+        var introWidth = $('.introduction').width(),
+        menuWidth = $('.menu').width();
+
+        $('.introduction').animate({
+            left: '-' + introWidth
+        }, 1000, 'easeOutQuart');
+        $('.menu').animate({
+            left: menuWidth
+        }, 1000, 'easeOutQuart', function () {
+            $('.home-page').css({
+                visibility: 'hidden'
+            });
+        });
+
+    });
+
+    //Page Onclick
+
+    $('.menu div.profile-btn').on('click', function () {
+        $('.profile-page').fadeIn(1200);
+    });
+
+    $('.menu div.resume-btn').on('click', function () {
+        $('.resume-page').fadeIn(1200);
+    });
+
+    $('.menu div.portfolio-btn').on('click', function () {
+        $('.portfolio-page').fadeIn(1200);
+    });
+
+    $('.menu div.contact-btn').on('click', function () {
+        $('.contact-page').fadeIn(1200);
+    });
+
+
+
+    $('.close-btn').on('click', function () {
+        $('.home-page').css({
+            visibility: 'visible'
+        });
+        $('.introduction, .menu').animate({
+            left: 0
+        }, 1000, 'easeOutQuart');
+        $('.profile-page, .resume-page, .portfolio-page, .contact-page').fadeOut(800);
+    });
+    
+    
+    
+    function maximizeHeight() {
+
+        var minHeight = 0;
+
+        $('.services').each(function () {
+
+            var maxHeight = $(this).height();
+
+            if (maxHeight > minHeight) {
+                minHeight = maxHeight;
+            }
+
+        });
+
+        $('.services').height(minHeight);
+    }
+
+    maximizeHeight();
+
+    $(window).on('resize', maximizeHeight);
+
+
+
+    $('.intro-content .social-media [data-toggle="tooltip"]').tooltip({
+        placement: 'bottom'
+    });
+
+    $('.contact-details .social-media [data-toggle="tooltip"]').tooltip();
+
+
+    $(function () {
+
+        var fillColor = $('.footer').css('background-color'); 
+
+
+
+        $('.skill').easyPieChart({
+             animate: 1000,
+            barColor: fillColor,
+            trackColor: '#c6c6c6',
+            scaleColor: '#c6c6c6',
+            scaleLength: 8,
+            lineWidth: 8,
+            size: 150,
+            lineCap: 'butt'
+        });
+
+    });
+
+
+
+    $(function () {
+
+        $('#projects').mixItUp({
+
+            callbacks: {
+                onMixLoad: function (state) {
+                    $('.portfolio-page').hide();
+                }
+            }
+
+        });
+
+    });
+
+
+
+    $(function () {
+        $('.show-popup').popup({
+            keepInlineChanges: true,
+            speed: 500
+        });
+    });
+
+
+
+
+
+    $(function () {
+        $(".owl-carousel").owlCarousel({
+            navigation: false, 
+            slideSpeed: 200,
+            paginationSpeed: 300,
+            singleItem: true
+        });
+    });
+
+
+
+
+    $('#contactForm').submit(function () {
+
+        $.ajax({
+            type: "POST",
+            url: "php/contact.php",
+            data: $('#contactForm').serialize(),
+            success: function (msg) {
+                if (msg == 'SEND') {
+                    $('.success').fadeIn();
+                    $('.error').fadeOut();
+                    $('#contactForm')[0].reset();
+                } else {
+                    $('.success').fadeOut();
+                    $('.error').fadeIn().find('h3').text(msg);
+                }
+            }
+        });
+        return false;
+    });
+
+
+
+
+
+
+    $('#preloader').delay(300).fadeOut('slow', function () {
+        $('.profile-page, .resume-page, .contact-page').hide();
+    });
+
+
+})(jQuery);
+
+
+
